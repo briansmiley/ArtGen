8b3f0f220212c6a31e5b4ed05a098271bf813b94
Brian Smiley <briantsmiley42@gmail.com>
2024-07-01 20:15:51 -0400

    adds redirects to signin

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,10 @@ export default function RootLayout({
       <html lang="en">
         <body>
           <SignedOut>
-            <SignInButton />
+            <SignInButton
+              forceRedirectUrl={"/api/userAuthCallback"}
+              signUpForceRedirectUrl={"/api/userAuthCallback"}
+            />
           </SignedOut>
           <SignedIn>
             <UserInfo />
