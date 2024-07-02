9151ceb20ef556cf0cee55c0165802773b9cb809
Brian Smiley <briantsmiley42@gmail.com>
2024-07-01 20:25:59 -0400

    adds comment

--- src/app/api/post/route.ts
+++ src/app/api/post/route.ts
@@ -1,7 +1,8 @@
 import { userReqHandler } from "@/app/middleware/userReqHandler";
 import prisma from "../../client";
 import { NextApiRequest, NextApiResponse } from "next";
-//
+
+//uses userReqHandler to get the userID and posts their block to the db
 export const post = userReqHandler(
   async (req: NextApiRequest, res: NextApiResponse) => {
     const color = req.body.color;
