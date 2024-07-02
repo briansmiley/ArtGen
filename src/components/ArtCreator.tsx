bce11bd6647c5f52cbf76393a74bab41a3ca2e39
Brian Smiley <briantsmiley42@gmail.com>
2024-07-02 14:34:07 -0400

    restyles and adds reroute

--- src/components/ArtCreator.tsx
+++ src/components/ArtCreator.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import ArtBlock from "./ArtBlock";
 import prisma from "../app/client";
+import { useRouter } from "next/navigation";
 const handleSubmit = async (color: string) => {
   try {
     const response = await fetch("/api/post", {
@@ -26,16 +27,29 @@ const handleSubmit = async (color: string) => {
 export default function ArtCreator() {
   //takes in a color from a controlled input and retnders an ArtBlock preview of the resulting color
   const [color, setColor] = useState("#000000");
-
+  const router = useRouter();
   return (
-    <div>
-      <input
-        type="color"
-        value={color}
-        onChange={e => setColor(e.target.value)}
-      />
+    <div className="flex flex-col w-full items-center">
       <ArtBlock color={color} />
-      <button onClick={() => handleSubmit(color)}>Submit</button>
+      <div className="flex items-center">
+        <label htmlFor="color">Color</label>
+        <input
+          type="color"
+          className="w-10 h-10"
+          value={color}
+          id="color"
+          onChange={e => setColor(e.target.value)}
+        />
+      </div>
+      <button
+        className="btn btn-blue mt-3"
+        onClick={() => {
+          handleSubmit(color);
+          router.push("/");
+        }}
+      >
+        Submit
+      </button>
     </div>
   );
 }
