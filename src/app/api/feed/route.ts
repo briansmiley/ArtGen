a1014bba3b286d57577067f98112a577ec797934
Brian Smiley <briantsmiley42@gmail.com>
2024-07-03 15:32:28 -0400

    reworks block fetching for cursor pagination

--- src/app/api/feed/route.ts
+++ src/app/api/feed/route.ts
@@ -2,25 +2,35 @@
 
 import prisma from "@/app/client";
 import { NextResponse } from "next/server";
+import { ArtBlock as ArtBlockSchema } from "@prisma/client";
 
-export async function GET(req: NextRequest) {
+export interface FeedGetResponse {
+  artBlocks: ArtBlockSchema[];
+  moreToFetch: boolean;
+}
+export async function GET(
+  req: NextRequest
+): Promise<NextResponse<FeedGetResponse | { error: string }>> {
   const params = req.nextUrl.searchParams;
-  const lastCreated =
-    params.get("lastCreated") === "null" ? null : params.get("lastCreated");
+  const lastId = params.get("lastId") === "null" ? null : params.get("lastId");
   const count = params.get("count");
-
+  console.log(params);
   try {
     const artBlocks = await prisma.artBlock.findMany({
-      where: {
-        createdAt: {
-          lt: lastCreated || undefined
-        }
-      },
-      orderBy: { createdAt: "desc" },
-      take: count ? parseInt(count) : 10
+      take: count ? parseInt(count) + 1 : 1, //take the number of blocks plus one to see if there's more to fetch
+      skip: lastId ? 1 : 0, //skip the first block if we're not starting from the beginning
+      cursor: lastId ? { id: lastId } : undefined, //start from the last block in the feed if we're not starting from the beginning
+      orderBy: { id: "desc" } //order by id in descending order
     });
-    console.log(artBlocks);
-    return NextResponse.json(artBlocks);
+    const data = artBlocks.slice(0, count ? parseInt(count) : 1); //slice off the extra block
+    console.log(
+      `Fetched ${artBlocks.length} blocks, sending back ${data.length}:`,
+      data
+    );
+    return NextResponse.json({
+      artBlocks: data,
+      moreToFetch: artBlocks.length > data.length
+    });
   } catch (error) {
     console.error("Error fetching ArtBlocks:", error);
     return NextResponse.json(
