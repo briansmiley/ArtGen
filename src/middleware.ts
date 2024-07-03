3254f2d01e308a237f6e11d766cd985680fe0364
Brian Smiley <briantsmiley42@gmail.com>
2024-07-03 15:08:22 -0400

    comments out middleware debug code

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,9 +5,9 @@ let reqRecord = {};
 // This Middleware does not protect any routes by default.
 // See https://clerk.com/docs/references/nextjs/clerk-middleware for more information about configuring your Middleware
 export default clerkMiddleware((auth, req) => {
-  console.log("Middling the wares", req.url);
-  reqRecord[req.url] = req.nextUrl.pathname;
-  console.log(reqRecord);
+  // console.log("Middling the wares", req.url);
+  // reqRecord[req.url] = req.nextUrl.pathname;
+  // console.log(reqRecord);
 });
 
 export const config = {
