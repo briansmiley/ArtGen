4829262abbdb04106c875a5f6ccc3495bfad3e31
Brian Smiley <briantsmiley42@gmail.com>
2024-07-03 13:42:17 -0400

    adds _next files to clerkMiddleware path

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,15 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+let reqRecord = {};
+
+// This Middleware does not protect any routes by default.
+// See https://clerk.com/docs/references/nextjs/clerk-middleware for more information about configuring your Middleware
+export default clerkMiddleware((auth, req) => {
+  console.log("Middling the wares", req.url);
+  reqRecord[req.url] = req.nextUrl.pathname;
+  console.log(reqRecord);
+});
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"]
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/_next(.*)", "/(api|trpc)(.*)"]
 };
