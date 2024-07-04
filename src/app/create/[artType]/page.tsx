bd17e49e7464c797400cc26b10ffac8ba3fac791
Brian Smiley <briantsmiley42@gmail.com>
2024-07-04 17:15:34 -0400

    fixes reference to ArtCreator block

--- src/app/create/[artType]/page.tsx
+++ src/app/create/[artType]/page.tsx
@@ -1,8 +1,7 @@
 //this page is the create page for the user to create their artblocks
 
 import { ArtBlockProps } from "@/components/ArtBlock";
-import ArtCreator from "@/components/ArtBlock/Color/ColorBlockControls";
-
+import ArtCreator from "@/components/ArtBlockCreator";
 type CreateProps = {
   params: { artType: ArtBlockProps["artParams"]["artType"] }; //derive the possible valies for artType from the ArtBlock component
 };
