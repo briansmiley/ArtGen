ab2e86e2833d8186ee16c8d24414f1b78a510619
Brian Smiley <briantsmiley42@gmail.com>
2024-07-05 17:20:01 -0400

    componentizes the create button and adds options

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import ArtFeed from "@/components/ArtFeed";
+import CreateButton from "@/components/CreateButton";
 import Image from "next/image";
 
 export default function Feed() {
@@ -7,11 +8,7 @@ export default function Feed() {
       Feed
       <ArtFeed />
       <div className="fixed bottom-5 right-5">
-        <a href="/create/color">
-          <button className="bg-purple-500 text-white rounded-full w-12 h-12 flex items-center justify-center text-2xl border-none cursor-pointer hover:shadow-lg pb-0.5">
-            +
-          </button>
-        </a>
+        <CreateButton />
       </div>
     </div>
   );
