82f9f224775f4582791dcd34d71c434b42ed3a49
Brian Smiley <briantsmiley42@gmail.com>
2024-07-06 14:12:01 -0400

    gets all the params interfaced and displaying (unrendered)

--- src/components/ArtBlock/Tree/TreeBlock.tsx
+++ src/components/ArtBlock/Tree/TreeBlock.tsx
@@ -1,15 +1,25 @@
 import { TreeBlockParams } from "@/services/ArtBlock.types";
+import { useEffect } from "react";
+import generateTree from "./generate";
 
 type TreeBlockProps = TreeBlockParams;
 
-export default function TreeBlock(params: TreeBlockProps) {
+const TreeBlock = (params: TreeBlockProps) => {
+  const branches = generateTree(params);
+  console.log(branches);
   return (
-    <div className="h-[300px] w-[300px]">
+    <div
+      className="h-[300px] w-[300px]"
+      style={{ backgroundColor: params.backgroundColor }}
+    >
+      {/*debug: just display all the param values*/}
       {Object.entries(params).map(([key, value]) => (
         <div key={key}>
           {key}: {value}
         </div>
       ))}
+      Tree Size: {branches.length}
     </div>
   );
-}
+};
+export default TreeBlock;
