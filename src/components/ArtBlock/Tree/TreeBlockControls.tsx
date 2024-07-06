0bbc75b5f554a793b06e76b2b90a71d80b142197
Brian Smiley <briantsmiley42@gmail.com>
2024-07-06 13:59:41 -0400

    adds first draft tree controls

--- src/components/ArtBlock/Tree/TreeBlockControls.tsx
+++ src/components/ArtBlock/Tree/TreeBlockControls.tsx
@@ -1,16 +1,78 @@
-import { TreeBlockParams } from "@/services/ArtBlock.types";
+import { TreeBlockParams, TreeParamRanges } from "@/services/ArtBlock.types";
 
-/**export interface TreeBlockParams {
-  artType: "tree";
-  branches: number;
-  tilt: number;
-  splitAngle: number;
-  minLength: number;
-} */
 export interface TreeBlockControlProps {
   artParams: TreeBlockParams;
   setParams: (params: TreeBlockParams) => void;
 }
-export default function TreeBlockControls() {
-  return <div>TreeBlockControls</div>;
+export default function TreeBlockControls({
+  artParams,
+  setParams
+}: TreeBlockControlProps) {
+  return (
+    <div>
+      {Object.entries(artParams).map(([key, value]) => {
+        if (typeof value === "number") {
+          return (
+            <ParamSlider
+              key={key}
+              label={key}
+              value={value}
+              setter={newValue => setParams({ ...artParams, [key]: newValue })}
+              min={TreeParamRanges[key].min}
+              max={TreeParamRanges[key].max}
+              step={TreeParamRanges[key].step}
+            />
+          );
+        }
+      })}
+      {["treeColor", "backgroundColor"].map(colorKey => (
+        <div key={colorKey}>
+          <label htmlFor={colorKey}>{colorKey}</label>
+          <input
+            type="color"
+            id={colorKey}
+            value={artParams[colorKey]}
+            onChange={e =>
+              setParams({ ...artParams, [colorKey]: e.target.value })
+            }
+          />
+        </div>
+      ))}
+    </div>
+  );
 }
+
+interface ParamSliderProps {
+  label: string;
+  value: number;
+  setter: (value: number) => void;
+  min: number;
+  max: number;
+  step: number;
+}
+/**Slider component for controlling a state variable
+ */
+const ParamSlider = ({
+  label,
+  value,
+  setter,
+  min,
+  max,
+  step
+}: ParamSliderProps) => {
+  return (
+    <div>
+      <label htmlFor={label}>{label}</label>
+      <input
+        type="range"
+        id={label}
+        min={min}
+        max={max}
+        step={step}
+        value={value}
+        onChange={e => setter(e.target.valueAsNumber)}
+      />
+      <span>{value}</span>
+    </div>
+  );
+};
