18edc37b1ad41c21c3f0d71d0cfba9555519abdf
Brian Smiley <briantsmiley42@gmail.com>
2024-07-09 11:42:04 -0400

    adds canvas scaling to fit tree

--- src/components/ArtBlock/Tree/Konvas.tsx
+++ src/components/ArtBlock/Tree/Konvas.tsx
@@ -34,9 +34,22 @@ const Konvas = (props: KonvasProps) => {
   const rightXPositiveSpace = width / 2 - boundaries.bottomRight.x; //for right, rightmost x=0 would mean we have half the width, subtract however far right it reaches
   const topYNegativeSpace = height - boundaries.topLeft.y; //0 height would leave {height} space, any protrusion up subtracts from that negative space
   const bottomYPositiveSpace = boundaries.bottomRight.y; //y=0 maps to bottom, so however high its lowest point isis how high above the bottom of canvas minY is
+  const treeTotalDims = {
+    width: boundaries.bottomRight.x - boundaries.topLeft.x,
+    height: boundaries.bottomRight.y - boundaries.topLeft.y
+  }; //total size of the tree bounding box
+  const xMidpoint = boundaries.topLeft.x + treeTotalDims.width / 2;
+  const canvasFitScaleFactor =
+    0.9 * Math.min(width / treeTotalDims.width, height / treeTotalDims.height); //scale factor to fit the tree in the canvas
   return (
     <Stage width={width} height={height}>
-      <Layer>
+      <Layer
+        scale={{ x: canvasFitScaleFactor, y: canvasFitScaleFactor }}
+        offset={{
+          x: -width / 2 / canvasFitScaleFactor + xMidpoint,
+          y: -height / canvasFitScaleFactor + 1.1 * bottomYPositiveSpace
+        }}
+      >
         {/* background */}
         <Rect
           x={0}
@@ -49,8 +62,8 @@ const Konvas = (props: KonvasProps) => {
         {branches.map((branch, idx) => (
           <Trapezoid
             key={`branches${idx}`}
-            x={branch.origin.x + width / 2}
-            y={branch.origin.y + height}
+            x={branch.origin.x}
+            y={branch.origin.y}
             startWidth={branch.startWidth}
             endWidth={branch.endWidth}
             length={branch.length}
