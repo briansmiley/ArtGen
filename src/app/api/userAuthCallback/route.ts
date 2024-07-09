bdca15e8ebac34b8b28f8531e2341b1fe04357fb
Brian Smiley <briantsmiley42@gmail.com>
2024-07-09 12:25:57 -0400

    adds dynamic mode to GET routes for deployment

--- src/app/api/userAuthCallback/route.ts
+++ src/app/api/userAuthCallback/route.ts
@@ -1,5 +1,6 @@
 import { userReqHandler } from "@/middleware/userReqHandler";
 import { redirect } from "next/navigation";
+export const mode = "dynamic";
 
 //this is the callback for the user to be redirected to after the user has been authenticated
 export const GET = userReqHandler(async req => {
