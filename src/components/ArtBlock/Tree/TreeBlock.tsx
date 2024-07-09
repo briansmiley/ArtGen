9523329c6a8569fcd2b37b9313a3896e5eba0364
Brian Smiley <briantsmiley42@gmail.com>
2024-07-09 11:51:14 -0400

    standardizes tree block canvas size

--- src/components/ArtBlock/Tree/TreeBlock.tsx
+++ src/components/ArtBlock/Tree/TreeBlock.tsx
@@ -7,10 +7,14 @@ type TreeBlockProps = TreeBlockParams;
 
 const TreeBlock = (params: TreeBlockProps) => {
   const { branches, boundaries } = generateTree(params);
+  const size = 500;
   return (
     <div
-      className="h-[700px] w-[700px]"
-      style={{ backgroundColor: params.backgroundColor }}
+      style={{
+        backgroundColor: params.backgroundColor,
+        width: size,
+        height: size
+      }}
     >
       {/*debug: just display all the param values*/}
       {/* {Object.entries(params).map(([key, value]) => (
@@ -22,8 +26,8 @@ const TreeBlock = (params: TreeBlockProps) => {
       <Konvas
         branches={branches}
         boundaries={boundaries}
-        width={700}
-        height={700}
+        width={size}
+        height={size}
         backgroundColor={params.backgroundColor}
       />
     </div>
