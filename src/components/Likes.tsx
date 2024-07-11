fd1bc8248df76b1e30b998139e8533ce640c2307
Brian Smiley <briantsmiley42@gmail.com>
2024-07-11 00:34:09 -0400

    restyles and adds some breakpoints

--- src/components/Likes.tsx
+++ src/components/Likes.tsx
@@ -30,12 +30,11 @@ export const Likes = (props: LikesProps) => {
   }, []);
   return (
     <div className="flex flex-row gap-1 items-center">
-      <span className="font-semibold ">{likesCount}</span>
+      <span className="font-semibold text-xs md:text-s">{likesCount}</span>
       <Heart
-        className={` cursor-pointer ${
+        className={` cursor-pointer w-3 h-3 md:w-4 md:h-4 ${
           isLiked ? "text-red-500" : "text-slate-700 hover:text-black"
         }`}
-        size={18}
         fill={isLiked ? "red" : "none"}
         onClick={async () => {
           //instantaneous optimistic update
